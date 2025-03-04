3ca0f7eeafe8bd343645b94d7faf67b131b69c26
CoJaCamp <CohenCampbel@gmail.com>
2025-03-04 02:00:52 -0500

    Added postTest Component, and .gitignore

--- frontend/src/routes/feed/+page.svelte
+++ frontend/src/routes/feed/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-	let postTest = [{}, {}, {}];
-</script>
-
 <div class="flex h-screen w-[100rem] flex-col items-center">
 	<div class="m-3 w-[40rem] border-2 border-[#c6b7be] bg-[#565a75] p-3 text-[#fafbf6] shadow-lg">
 		<h2 class="mb-2 flex justify-center text-4xl">About</h2>
---
+++ frontend/src/routes/feed/postTest.svelte
@@ -0,0 +1,25 @@
+<script>
+	let postTest = [
+		{
+			tags: ['green', 'purple', 'nier', 'dark', 'minimalism', 'yorha', 'nier:automata'],
+			Uploader: 'user1',
+			Favorites: 21,
+			artist: 'Unknown',
+			link: 'https://wallhaven.cc/w/kxpyy1'
+		},
+		{
+			tags: ['big ben', 'clock tower', 'uk', 'landmark', 'london'],
+			Uploader: 'user2',
+			Favorites: 31,
+			artist: 'Joe Man',
+			link: 'https://wallhaven.cc/w/gjd2z7'
+		},
+		{
+			tags: ['science', 'quote', 'text', 'typography', 'digital art', 'simple'],
+			Uploader: 'user1',
+			Favorites: 18,
+			artist: 'JohnMan2',
+			link: 'https://wallhaven.cc/w/nexddr'
+		}
+	];
+</script>
