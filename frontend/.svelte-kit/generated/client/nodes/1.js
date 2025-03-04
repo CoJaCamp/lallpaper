5f3c10117a2ffa27893f8e1008634af3ce6f3c4b
CoJaCamp <126370490+CoJaCamp@users.noreply.github.com>
2025-03-04 02:07:54 -0500

    Delete frontend/.svelte-kit directory

--- frontend/.svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.17.3_@sveltejs+vite-plugin-svelte@5.0.3_svelte@5.20.5_vite@6.2.0_jiti@2_74263bf8230f8bc2632729ee942eb46d/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
