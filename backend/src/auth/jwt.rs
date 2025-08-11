f0f041b62e8eece50a08bc943209183c3d37324a
CoJaCamp <CohenCampbel@gmail.com>
2025-08-11 15:15:54 -0400

    Created encode_access_token function; Creates access tokens that expire after 1 hour

--- backend/src/auth/jwt.rs
+++ backend/src/auth/jwt.rs
@@ -1,35 +1,50 @@
-//use chrono::{Duration, Utc}; Only needed for experation
+use chrono::{Duration, Utc};
 use jsonwebtoken::{DecodingKey, EncodingKey, Header, TokenData, Validation, decode, encode};
 use serde::{Deserialize, Serialize};
+
 use std::env;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Claims {
-    pub sub: i32, // User ID
-                  // pub exp: usize, //!!For the sake of simplicity the experation has been omitted, add back if using in production!!
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum TokenType {
+    Access,
+    Refresh,
 }
 
-pub fn create_jwt(user_id: i32) -> Result<String, jsonwebtoken::errors::Error> {
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AccessClaims {
+    pub user_id: i32, //user id
+    pub username: String,
+    pub user_roles: Vec<String>,
+    pub exp: u64,
+    pub token_type: TokenType,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct RefreshClaims {
+    pub user_id: i32,
+    pub exp: u64,
+    pub token_type: TokenType,
+}
+
+pub fn encode_access_token(user_id: i32, username: String, user_roles: Vec<String>) -> Result<(String, u64), jsonwebtoken::errors::Error> {
     let secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set in .env");
-    let claim = Claims { sub: user_id };
 
-    let token = encode(
-        &Header::default(),
-        &claim,
-        &EncodingKey::from_secret(secret.as_bytes()),
-    );
+    let expiration = Utc::now().checked_add_signed(Duration::hours(1)).expect("valid timestamp").timestamp() as u64;
 
-    token
+    let access_claim = AccessClaims{
+        user_id,
+        username,
+        user_roles,
+        exp: expiration,
+        token_type: TokenType::Access,
+    }
+
+    let new_token = encode(&Header::default(), &access_claim, &EncodingKey::from_secret(&secret.as_bytes()))?;
+
+    Ok((new_token, expiration))
 }
 
 pub fn validate_jwt(token: &str) -> Result<TokenData<Claims>, jsonwebtoken::errors::Error> {
     let secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set in .env");
-
-    let decoded = decode::<Claims>(
-        &token,
-        &DecodingKey::from_secret(secret.as_bytes()),
-        &Validation::default(),
-    );
-
-    decoded
 }
